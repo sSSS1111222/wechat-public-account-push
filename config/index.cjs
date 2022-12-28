1e40ab6b33d494c37fdd10a2a906f5ba77ab3aa0
sSSS1111222 <121558274+sSSS1111222@users.noreply.github.com>
2022-12-29 00:06:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oYBlV6DPQifoneILF4cFNgH0Kd8U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lsLwTqPObuWQcbPrmjqEBxxs_6QNDM6H-CSCPndrzlc',
+      useTemplateId: 'E4aHEUt9J8kuzEy3BCgB50GrXPU_d4i3GVOQuMsOnrQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-22',
       PROVINCE: '广东',
@@ -38,7 +38,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oYBlV6PZjWXldRmY1PLEJb3pdOY8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lsLwTqPObuWQcbPrmjqEBxxs_6QNDM6H-CSCPndrzlc',
+      useTemplateId: 'E4aHEUt9J8kuzEy3BCgB50GrXPU_d4i3GVOQuMsOnrQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-22',
       PROVINCE: '甘肃省',
